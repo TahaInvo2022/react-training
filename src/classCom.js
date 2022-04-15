28c8567de958f372c28d24834ad50461d1186000
Taha Shahid <tahashahid@192.168.10.16>
2022-04-15 22:47:18 +0500

    react training start function component and class componenet

---
+++ src/classCom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class ClaCom extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+                <h1>This is class component</h1>
+            </div>
+        );
+    }
+}
+ 
+export default ClaCom;
---
+++ src/dynamicText.js
@@ -0,0 +1,11 @@
+const dynamic = () => {
+    return (
+        <div>
+            <h1>{"Hello taha how are you?"}</h1>
+            <p>{"i am fine what about you?"}</p>
+            <p>{"i am also fine "}</p>
+        </div>
+    );
+}
+
+export default dynamic;
